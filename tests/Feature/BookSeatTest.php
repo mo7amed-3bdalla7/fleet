7106966d59094eaa6b6335d79954f93c08984234
Mohamed Abdallah <mo7amed.3bdalla7@gmail.com>
2020-05-23 20:37:55 +0400

    Write feature test for book seat api endpoint

---
+++ tests/Feature/BookSeatTest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Trip;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class BookSeatTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function testWithoutRequestBody()
+    {
+        $this->json('POST', '/api/book-seats')
+            ->assertStatus(422);
+    }
+
+    public function testWrongStationsIds()
+    {
+
+        $this->json(
+            'POST',
+            '/api/book-seats',
+            ['start' => 1, 'end' => 1]
+        )->assertStatus(422);
+
+        $this->json(
+            'POST',
+            '/api/book-seats',
+            ['start' => -1, 'end' => 1]
+        )->assertStatus(422);
+    }
+
+
+    public function testValidRequest()
+    {
+        $this->json(
+            'POST',
+            '/api/book-seats',
+            ['start' => 1, 'end' => 5]
+        )->assertStatus(200)
+            ->assertJsonPath('ticket.id', 1);
+    }
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->seed();
+    }
+
+}
